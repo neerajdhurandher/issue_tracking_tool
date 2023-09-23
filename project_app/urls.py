bb610b03bf8082c4cbd7e4d63016abfc7186d0f5
neerajdhurandher-deloitte <101085508+neerajdhurandher-deloitte@users.noreply.github.com>
2023-09-23 13:48:27 +0530

    modify in project endpoints & move project views in project folder

--- project_app/urls.py
+++ project_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import Project
+from .project.views_project import Project, ProjectByID
 
 urlpatterns = [
     path("projects", Project.as_view(), name="Projects"),
-    path("projects/<str:project_id>", Project.as_view(), name="Delete Project")
+    path("project/<str:project_id>", ProjectByID.as_view(), name="Project by ID"),
 
 ]
