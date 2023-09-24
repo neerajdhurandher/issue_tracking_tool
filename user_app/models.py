38adac3615267c0360fd362fbf6a5a10f0fa02ce
neerajdhurandher-deloitte <101085508+neerajdhurandher-deloitte@users.noreply.github.com>
2023-09-24 22:38:28 +0530

    fix delete user issue & modify user id template

--- user_app/models.py
+++ user_app/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.hashers import make_password
 
 # Create your models here.
 
 
 class User(AbstractUser):
+    id = models.CharField(max_length=100,
+                          unique=True, editable=False, primary_key=True)
     organization_name = models.CharField(max_length=255,null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            last_user_id = self.__class__.objects.order_by(
+                '-id').values_list('id', flat=True).first()
+            if last_user_id:
+                last_user_id = int(
+                    last_user_id.split('_')[2])
+                self.id = f'user_id_{last_user_id + 1}'
+            else:
+                self.id = 'user_id_1'
+        return super().save(*args, **kwargs)
--- user_app/views.py
+++ user_app/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from .serializer import User
 from .user_utils import UserUtils as user_utils
+from .models import User as UserModel
 import logging
 logger = logging.getLogger(__name__)
 
@@ -63,16 +64,10 @@ class GetUserByID(APIView):
             return Response(user_details, status=status.HTTP_200_OK)
 
     def delete(self, request, user_id):
-        logger.info("inside delete fun.")
-        user = user_utils.check_user_existence(id=user_id)
-
-        if user is None:
+        try:
+            user_obj = UserModel.objects.get(id=user_id)
+            user_obj.delete()
+            return Response({"success": "yes"}, status=status.HTTP_200_OK)
+        except UserModel.DoesNotExist:
             return Response({"error": "The user does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
-        try:
-            user.delete()
-            return Response({"success": "yes"}, status=status.HTTP_200_OK)
-        except Exception as error:
-            logging.error(f"Exception in delete user API: {error}")
-            return Response("Error while deleting user.", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
